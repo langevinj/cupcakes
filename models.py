dc487d591846f5dd9a951885875d3f720befceb3
Jeremy Langevin <langevinj832@gmail.com>
2020-09-14 09:25:13 -0400

    created cupcake class, basic app and models setup

--- models.py
+++ models.py
@@ -1,10 +1,24 @@
 """Models for Cupcake app."""
 
-from flask import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app 
+    db.init_app(app)
+
+DEFAULT_IMAGE_URL = "https://thestayathomechef.com/wp-content/uploads/2017/12/Most-Amazing-Chocolate-Cupcakes-1-small.jpg"
 
 class Cupcake(db.Model):
     """Cupcakes"""
 
-    __tablename__="cupcakes"
+    __tablename__= "cupcakes"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
